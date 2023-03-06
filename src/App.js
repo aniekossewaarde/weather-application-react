9759024de5289c03903cfdaad4c1e8c91188251e
aniekossewaarde <116111669+aniekossewaarde@users.noreply.github.com>
2023-03-06 16:39:53 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       <Weather />
       </header>
+      </div>
     </div>
   );
 }
