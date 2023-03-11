9016d1debd18d669af4c6f7cd108c3050ef19812
aniekossewaarde <116111669+aniekossewaarde@users.noreply.github.com>
2023-03-11 20:55:53 +0100

    sorted error in timestamp

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -4,7 +4,7 @@ export default function FormattedDate(props) {
 let days =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 let day = days[props.date.getDay()];
 let hours = props.date.getHours();
-if (hours = < 10) {
+if (hours < 10) {
     hours = `0${hours}`;
 }
 let minutes = props.date.getMinutes();
